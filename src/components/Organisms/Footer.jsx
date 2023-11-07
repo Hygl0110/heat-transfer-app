2b3f7d468a43124ceeeb44433407fcf7a1e0cf4c
danie <danielcardona0110@outlook.com>
2023-11-07 13:40:13 -0500

    Correcciones

--- src/components/Organisms/Footer.jsx
+++ src/components/Organisms/Footer.jsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer>
       <label>Developments:</label>
       <p>
-        Daniel Herney Cardona Cardona Jaramillo <br />
+        Daniel Herney Cardona Jaramillo <br />
         Mateo Montoya Uribe <br />
         Iván Alexis Londoño Patiño <br />
       </p>
@@ -14,8 +14,8 @@ export const Footer = () => {
       </p>
       <label>Professor:</label> Gustavo Adolfo Patiño Jaramillo <br />
       <label>
-        Instituto Tecnológico Metropolitano <br /> Facultad de Ingeniería
-        Ingeniería y Electromecánica <br />
+        Instituto Tecnológico Metropolitano <br />
+        Facultad de Ingeniería y Electromecánica <br />
         Medellín <br />
         2023
       </label>
